5e0b9410c5b788f5c66319811396de8b572dcb2b
tatazoe <takashi.tazoe@gmail.com>
2022-12-06 07:57:00 +0900

    Get /greeting feature

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -9,4 +12,11 @@ public class HelloController {
     public String index() {
         return "hello";
     }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(required = false) String message, Model model) {
+        model.addAttribute("sample", message);
+        return "hello";
+    }
+
 }
