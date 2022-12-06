9ebf2b08454b19c5b8fbbd08d7977f3886b5f104
tatazoe <takashi.tazoe@gmail.com>
2022-12-06 11:37:40 +0900

    :feature Post request support

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,21 +2,28 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/")
-    public String index() {
-        return "hello";
-    }
-
     @GetMapping("/greeting")
     public String greeting(@RequestParam(required = false) String message, Model model) {
         model.addAttribute("sample", message);
         return "hello";
+    }
+
+    @GetMapping("/")
+    public String index(){
+        return "form";
+    }
+
+    @PostMapping("/confirm")
+    public String confirm(@RequestParam String message, Model model) {
+        model.addAttribute("message", message);
+        return "confirm";
     }
 
 }
